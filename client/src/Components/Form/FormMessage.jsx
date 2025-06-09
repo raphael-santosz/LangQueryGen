270f7a05ca3eced0b97058e26a06f3c387fcf99e
Rafael Azzolini <rafaazzolini@hotmail.com>
2025-06-09 18:42:18 +0200

    Fixing the logged in user name

--- client/src/Components/Form/FormMessage.jsx
+++
@@ -1,8 +0,0 @@
-// src/components/Form/FormMessage.jsx
-import React from 'react';
-
-const FormMessage = ({ message }) => (
-  <p className="text-red-500 text-xs italic transition-opacity duration-500">{message}</p>
-);
-
-export default FormMessage;
